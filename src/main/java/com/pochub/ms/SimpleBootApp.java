fadee94b7c9b5c88316529e930f68c4cc7a65ae1
snmaddula <snmaddula@outlook.com>
2019-01-29 06:00:52 +0530

    Initial Commit

---
+++ src/main/java/com/pochub/ms/SimpleBootApp.java
@@ -0,0 +1,12 @@
+package com.pochub.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBootApp.class, args);
+	}
+}
